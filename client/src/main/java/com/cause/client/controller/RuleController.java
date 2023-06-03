a3c191e5582cae647ad167a9086e33592fad287a
songzhipeng <songzhipeng@loopin.group>
2023-06-03 12:20:33 +0800

    first commit

---
+++ client/src/main/java/com/cause/client/controller/RuleController.java
@@ -0,0 +1,39 @@
+package com.cause.client.controller;
+
+import com.bstek.urule.Utils;
+import com.bstek.urule.runtime.KnowledgePackage;
+import com.bstek.urule.runtime.KnowledgeSession;
+import com.bstek.urule.runtime.KnowledgeSessionFactory;
+import com.bstek.urule.runtime.service.KnowledgeService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Desc
+ *
+ * @author zengxzh@yonyou.com
+ * @version V1.0.0
+ * @date 2018/1/4
+ */
+@RestController
+public class RuleController {
+    @RequestMapping("rule")
+    public String rule(@RequestParam String time, @RequestParam String price) throws IOException {
+        //创建一个KnowledgeSession对象
+        KnowledgeService knowledgeService = (KnowledgeService) Utils.getApplicationContext().getBean(KnowledgeService.BEAN_ID);
+        KnowledgePackage knowledgePackage = knowledgeService.getKnowledge("test/13");
+        KnowledgeSession session = KnowledgeSessionFactory.newKnowledgeSession(knowledgePackage);
+
+        Map<String, Object> param = new HashMap();
+        param.put("time", time);
+        param.put("price", price);
+        session.fireRules(param);
+
+        return String.valueOf(session.getParameter("commission"));
+    }
+}
